c6613a57eb2a1d661b3f4956be2ef569504fa906
waletee <taiwobiodun@gmail.com>
2023-01-06 20:35:38 +0100

    hot fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """This module instantiates an instance of the Storage will be used"""
 
 from os import getenv
--- models/place.py
+++ models/place.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ Place Module for HBNB project """
 
 from models.amenity import Amenity
